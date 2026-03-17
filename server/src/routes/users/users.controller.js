fa0dac0e2509f58bc4d6e2139c45e3633b35a1f5
gidado662003 <gidadolateef6@gmail.com>
2026-03-17 09:56:50 +0100

    updated client

--- server/src/routes/users/users.controller.js
+++ server/src/routes/users/users.controller.js
@@ -146,6 +146,7 @@ async function login(req, res) {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
+      sameSite: "none",
       path: "/",
       maxAge: 24 * 60 * 60 * 1000,
     });
